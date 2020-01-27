540f343663977790448378244bfe715232adc32d
Mateusz Paprocki <mattpap@gmail.com>
2020-01-27 11:52:20 +0100

    Redesign resource resolving and management

--- bokeh/core/templates.py
+++ bokeh/core/templates.py
@@ -10,11 +10,9 @@ models in various ways.
 .. bokeh-jinja:: bokeh.core.templates.AUTOLOAD_JS
 .. bokeh-jinja:: bokeh.core.templates.AUTOLOAD_NB_JS
 .. bokeh-jinja:: bokeh.core.templates.AUTOLOAD_TAG
-.. bokeh-jinja:: bokeh.core.templates.CSS_RESOURCES
 .. bokeh-jinja:: bokeh.core.templates.DOC_JS
 .. bokeh-jinja:: bokeh.core.templates.DOC_NB_JS
 .. bokeh-jinja:: bokeh.core.templates.FILE
-.. bokeh-jinja:: bokeh.core.templates.JS_RESOURCES
 .. bokeh-jinja:: bokeh.core.templates.NOTEBOOK_LOAD
 .. bokeh-jinja:: bokeh.core.templates.PLOT_DIV
 .. bokeh-jinja:: bokeh.core.templates.ROOT_DIV
@@ -48,12 +46,10 @@ __all__ = (
     'AUTOLOAD_JS',
     'AUTOLOAD_NB_JS',
     'AUTOLOAD_TAG',
-    'CSS_RESOURCES',
     'DOC_JS',
     'DOC_NB_JS',
     'FILE',
     'get_env',
-    'JS_RESOURCES',
     'MACROS',
     'NOTEBOOK_LOAD',
     'PLOT_DIV',
@@ -87,10 +83,6 @@ _env.filters['json'] = lambda obj: Markup(json.dumps(obj))
 #-----------------------------------------------------------------------------
 # General API
 #-----------------------------------------------------------------------------
-
-JS_RESOURCES = _env.get_template("js_resources.html")
-
-CSS_RESOURCES = _env.get_template("css_resources.html")
 
 SCRIPT_TAG = _env.get_template("script_tag.html")
 
